2a1cec21e2030d6bca3df6fb6646eeb4d46c9e46
holutahyour <holutahyour01@gmail.com>
2022-05-03 07:19:37 -0700

    Write a function that allocates memory using malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -8,4 +8,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif /* _HOLBERTON_H_ */
+#endif /* _MAIN_H_ */
